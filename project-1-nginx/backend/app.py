dd3ca00842cbc07205087041f1864152d5934eba
Nika <nika.giqoshvili@gmail.com>
2026-07-23 22:42:33 -0400

    Add backend API and Nginx reverse proxy

---
+++ project-1-nginx/backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.get("/api")
+def api():
+    return jsonify(
+        message="Backend API is working",
+        project="Nika's DevOps Portfolio",
+        status="success"
+    )
+
+
+@app.get("/health")
+def health():
+    return jsonify(status="healthy"), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
