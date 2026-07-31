a4e0a5abe1e2cbdf2abe0b946d4167108f66b0ba
Nika Giqo <YOUR_GITHUB_EMAIL>
2026-07-31 13:37:03 +0400

    Add Azure public IP NIC and secirity rules

--- project-3-azure-terraform/main.tf
+++ project-3-azure-terraform/main.tf
@@ -34,3 +34,53 @@ resource "azurerm_subnet_network_security_group_association" "application" {
   subnet_id                 = azurerm_subnet.application.id
   network_security_group_id = azurerm_network_security_group.application.id
 }
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "Allow-SSH-From-Home"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = var.allowed_ssh_source
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.devops.name
+  network_security_group_name = azurerm_network_security_group.application.name
+}
+resource "azurerm_network_security_rule" "http" {
+  name                        = "Allow-HTTP"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.devops.name
+  network_security_group_name = azurerm_network_security_group.application.name
+}
+resource "azurerm_public_ip" "vm" {
+  name                = var.public_ip_name
+  location            = azurerm_resource_group.devops.location
+  resource_group_name = azurerm_resource_group.devops.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+
+  tags = azurerm_resource_group.devops.tags
+}
+resource "azurerm_network_interface" "vm" {
+  name                = var.network_interface_name
+  location            = azurerm_resource_group.devops.location
+  resource_group_name = azurerm_resource_group.devops.name
+
+  ip_configuration {
+    name                          = "primary"
+    subnet_id                     = azurerm_subnet.application.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm.id
+  }
+
+  tags = azurerm_resource_group.devops.tags
+}
